66d5da361d628a56c38f11719ede6cfccd877075
Neto Melo <netomelo1998@gmail.com>
2024-08-17 00:52:26 -0300

    Add files via upload

---
+++ link-na-bio/README.md
@@ -0,0 +1,2 @@
+# link-na-bio
+ Link na Bio
---
+++ scripts.js
@@ -0,0 +1,7 @@
+const pag = document.querySelector("#circle")
+
+pag.addEventListener("click", (evento) => {
+    evento.preventDefault()
+
+    window.location.href = "./index.html"
+})
